0dd14a11ca0df04f7efff50a481889c22a17d1eb
Marten Krings <mkrings@thinkwisesoftware.com>
2019-06-04 17:23:34 +0200

    troubleshooting

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ window.onload = () => {
     askPermission().then(() => {
         if ('serviceWorker' in navigator) {
             navigator.serviceWorker.register('service-worker.js').then(registration => {
+                const messaging = firebase.messaging();
                 messaging.useServiceWorker(registration)
                 handlePushMessage();
             });
