c9674ede1e4bd696b724de02d9ccf28062ded236
Marten Krings <mkrings@thinkwisesoftware.com>
2019-06-04 17:01:38 +0200

    troubleshooting

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ window.onload = () => {
     askPermission().then(() => {
         handlePushMessage();
 
-        if ('serviceWorker' in navigator) {
-            // Use the window load event to keep the page load performant
-            navigator.serviceWorker.register('service-worker.js');
-        }
+        // if ('serviceWorker' in navigator) {
+        //     // Use the window load event to keep the page load performant
+        //     navigator.serviceWorker.register('service-worker.js');
+        // }
     });
 }
     
