3ab3f77263ec8c049f030164812ef3cc2186498c
Marten Krings <mkrings@thinkwisesoftware.com>
2019-06-04 16:29:51 +0200

    troubleshooting

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ import push from './notifications/push';
 
 window.onload = () => {
     push.initializeFirebase();
-    push.askPermission().then(() => {
-        push.handlePushMessage();
+    // push.askPermission().then(() => {
+    //     push.handlePushMessage();
 
-        if ('serviceWorker' in navigator) {
-            // Use the window load event to keep the page load performant
-            window.addEventListener('load', () => {
-                navigator.serviceWorker.register('service-worker.js');
-            });
-        }
-    });
+    //     if ('serviceWorker' in navigator) {
+    //         // Use the window load event to keep the page load performant
+    //         window.addEventListener('load', () => {
+    //             navigator.serviceWorker.register('service-worker.js');
+    //         });
+    //     }
+    // });
 }
