c62290ad4f63489018427a747e5614d941661788
Marten Krings <mkrings@thinkwisesoftware.com>
2019-06-04 18:25:57 +0200

    troubleshooting

--- index.js
+++ index.js
@@ -4,7 +4,12 @@ window.onload = () => {
         if ('serviceWorker' in navigator) {
             navigator.serviceWorker.register('service-worker.js').then(registration => {
                 const messaging = firebase.messaging();
-                messaging.useServiceWorker(registration)
+                messaging.useServiceWorker(registration);
+
+                const token = await messaging.getToken();
+                console.log('token: ', token);
+                //send token to server
+
                 handlePushMessage();
             });
         }
--- notifications/push.js
+++ notifications/push.js
@@ -9,12 +9,7 @@ const askPermission = async () => {
     try {
         const messaging = firebase.messaging();
         await messaging.requestPermission();
-        const token = await messaging.getToken();
-        console.log('token: ', token);
-        
-        //send token to server
-
-        return token;
+        return;
     } catch (error) {
         console.error(error);
     }
