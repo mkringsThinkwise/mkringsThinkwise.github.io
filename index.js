7304d8e34e7b9bc1f03709b86090dfd8ee9887a8
Marten Krings <mkrings@thinkwisesoftware.com>
2019-06-04 16:55:26 +0200

    troubleshooting

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 window.onload = () => {
     initializeFirebase();
-    // push.askPermission().then(() => {
-    //     push.handlePushMessage();
+    push.askPermission().then(() => {
+        push.handlePushMessage();
 
-    //     if ('serviceWorker' in navigator) {
-    //         // Use the window load event to keep the page load performant
-    //         window.addEventListener('load', () => {
-    //             navigator.serviceWorker.register('service-worker.js');
-    //         });
-    //     }
-    // });
+        if ('serviceWorker' in navigator) {
+            // Use the window load event to keep the page load performant
+            window.addEventListener('load', () => {
+                navigator.serviceWorker.register('service-worker.js');
+            });
+        }
+    });
 }
