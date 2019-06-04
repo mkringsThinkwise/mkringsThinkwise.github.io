ee863137366ccc78360fc70d7758a3a2f98664b0
Marten Krings <mkrings@thinkwisesoftware.com>
2019-06-04 16:58:20 +0200

    troubleshooting

--- index.js
+++ index.js
@@ -5,9 +5,8 @@ window.onload = () => {
 
         if ('serviceWorker' in navigator) {
             // Use the window load event to keep the page load performant
-            window.addEventListener('load', () => {
-                navigator.serviceWorker.register('service-worker.js');
-            });
+            navigator.serviceWorker.register('service-worker.js');
         }
     });
 }
+    
