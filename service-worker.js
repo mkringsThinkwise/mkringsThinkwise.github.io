b9f5ba20624cb6675007651a2495ebb226fe35b0
Marten Krings <mkrings@thinkwisesoftware.com>
2019-06-04 18:34:20 +0200

    offline support

--- service-worker.js
+++ service-worker.js
@@ -35,3 +35,46 @@ messaging.setBackgroundMessageHandler(function (payload) {
     return self.registration.showNotification(notificationTitle,
         notificationOptions);
 });
+
+const filesToCache = [
+    '/',
+    'index.html',
+    'index.js',
+    'notifications/config.js',
+    'notifications/push.js',
+];
+
+const staticCacheName = 'pages-cache-v1';
+
+self.addEventListener('install', event => {
+    console.log('Attempting to install service worker and cache static assets');
+    event.waitUntil(
+      caches.open(staticCacheName)
+      .then(cache => {
+        return cache.addAll(filesToCache);
+      })
+    );
+  });
+
+
+  self.addEventListener('fetch', event => {
+    console.log('Fetch event for ', event.request.url);
+    event.respondWith(
+      caches.match(event.request)
+      .then(response => {
+        if (response) {
+          console.log('Found ', event.request.url, ' in cache');
+          return response;
+        }
+        console.log('Network request for ', event.request.url);
+        return fetch(event.request)
+  
+        // TODO 4 - Add fetched files to the cache
+  
+      }).catch(error => {
+  
+        // TODO 6 - Respond with custom offline page
+  
+      })
+    );
+  });
